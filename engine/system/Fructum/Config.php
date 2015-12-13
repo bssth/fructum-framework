344aaca6ffe84883c52147d42e345a08cd4aa5a4
Boris Stelmah <mikael.chip@gmail.com>
2015-12-13 22:18:21 +0200

    added config class

---
+++ engine/system/Fructum/Config.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * This class includes default configuration. To change it, use hooks or extensions
+	 *
+	 * @author Mike Chip
+	 */
+	 
+	namespace Fructum;
+	
+	class Config
+	{
+		
+		const cache = '\Memcache';
+		const sql_host = '127.0.0.1';
+		const sql_user = 'test2';
+		const sql_password = 'test2';
+		const sql_database = 'test2';
+		const sql_unique = 'id';
+		
+	}
