afd6ccdd6e28c2bf93c115dad75537ffc1a590a5
Boris Stelmah <mikael.chip@gmail.com>
2015-12-19 14:27:26 +0200

    moved benchmark class to extension

--- engine/extensions/web_test/Controller/index.php
+++ engine/extensions/web_test/Controller/index.php
@@ -5,22 +5,18 @@
 	 
 	namespace Controller;
 	
-	use Application\Benchmark;
+	use Unittest\Benchmark;
 
 	class Index
 	{
 		/**
-		 * Just send string with script name and server signature to client
+		 * Just send string with script name, server signature and loading time to client
 		 * 'return' in controller means 'send to output'
 		 */
 		function actionIndex()
 		{
 			$benchmark = Benchmark::i();
-			$i = 0;
-			while($i < 1000){
-				echo $i."<br>";
-				$i++;
-			}
+
 			return "<h3>Fructum works! This controller is placed in following file: <b>". __FILE__ ."</b></h3> {$_SERVER['SERVER_SIGNATURE']} <br>Est. time: ".$benchmark->est_time();
 		}
 	}
