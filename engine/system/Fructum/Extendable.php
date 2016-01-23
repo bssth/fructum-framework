a459defee54b13b7efdca8e842cf071f8ee4f268
Boris Stelmah <mikael.chip@gmail.com>
2016-01-24 00:36:59 +0200

    added extendable class

---
+++ engine/system/Fructum/Extendable.php
@@ -0,0 +1,45 @@
+<?php
+	/**
+	 * Class provides dynamic function creating
+	*/
+	
+	namespace Fructum;
+	
+	class Extendable
+	{
+		
+		protected $_functions = array();
+		
+		/** 
+		 * Creates dynamic function. Notice that class instance will always be given in last argument of function and you can't use $this variable
+		 * @param string $fname 
+		 * @param callable $function
+		 */
+		public function addFunction($fname, $function)
+		{
+			if(!is_string($fname)) { throw new Exception("Function name must be string"); }
+			if(!is_callable($function)) { throw new Exception("Function must be callable"); }
+			if(isset($this->_functions[$fname])) { throw new Exception("Trying to rewrite function, aborting"); }
+			
+			$this->_functions[$fname] = $function;
+		}
+		
+		/** 
+		 * Calls dynamic-created function
+		 *
+		 * @param string $f 
+		 * @param array $args 
+		 * @return mixed
+		 */
+		public function __call($f, $args)
+		{
+			if(isset($this->_functions[$f])) {
+				$args[] = $this;
+				return call_user_func_array($this->_functions[$f], $args);
+			}
+			else {
+				return NULL;
+			}
+		}
+		
+	}
