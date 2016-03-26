4fd0a9e8da94c9197fcd89b50d42da80ce9a9eaf
Boris Stelmah <mikael.chip@gmail.com>
2016-03-26 20:21:32 +0200

    autoloader root fixes

--- engine/system/autoload.php
+++ engine/system/autoload.php
@@ -3,12 +3,10 @@
 	/**
 	 * Fructum autoloader inits root and starts core
 	 *
-	 * @version 1.0
+	 * @version 1.1
 	 * @author Mike Chip
 	 */
 	
-	define('\Fructum\ROOT', __DIR__ . '/../'); // defines engine root 
-		
 	require_once(__DIR__ . '/Fructum/Core.php'); // loading core 
 		
 	\Fructum\Core::init(); // init core
