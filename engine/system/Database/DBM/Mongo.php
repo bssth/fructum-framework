f89cc74176a5c4f6b9989a3992ab4be5cc4abe46
Boris Stelmah <mikael.chip@gmail.com>
2016-07-16 22:08:58 +0300

    additional check in mongo dbm

--- engine/system/Database/DBM/Mongo.php
+++ engine/system/Database/DBM/Mongo.php
@@ -13,7 +13,14 @@
 		
 		public function __construct()
 		{
-			$this->i = \Database\Mongo::i(); // saves instance of MongoClient object with active connection
+			if(\Fructum\Config::db_user != null)
+			{
+				$this->i = new \MongoClient('mongodb://' . \Fructum\Config::db_user . ':' . \Fructum\Config::db_password . '@' . \Fructum\Config::db_host);
+			}
+			else
+			{
+				$this->i = new \MongoClient(\Fructum\Config::db_host);
+			}
 		}
 		
 		public function __get($k)
@@ -30,12 +37,22 @@
 		public function findOne($criteria)
 		{
 			\Debug\Fuse::addData('mongo_queries', 1);
+			
+			if(isset($criteria['_id']) and !is_object($criteria['_id'])) {
+				$criteria['_id'] = new \MongoId($criteria['_id']);
+			}
+			
 			return $this->i->{$this->table}->findOne($criteria);
 		}
 		
 		public function find($criteria)
 		{
 			\Debug\Fuse::addData('mongo_queries', 1);
+			
+			if(isset($criteria['_id']) and !is_object($criteria['_id'])) {
+				$criteria['_id'] = new \MongoId($criteria['_id']);
+			}
+			
 			return $this->i->{$this->table}->find($criteria);
 		}
 		
@@ -48,18 +65,21 @@
 		public function update($cr, $data)
 		{
 			\Debug\Fuse::addData('mongo_queries', 1);
-			return $this->i->{$this->table}->update($cr, $data);
+			
+			if(isset($cr['_id']) and !is_object($cr['_id'])) {
+				$cr['_id'] = new \MongoId($cr['_id']);
+			}
+			if(isset($data['_id']) and !is_object($data['_id'])) {
+				$data['_id'] = new \MongoId($data['_id']);
+			}
+			
+			return $this->i->{$this->table}->update($cr, $data)['nModified'];
 		}
 		
 		public function remove($cr)
 		{
 			\Debug\Fuse::addData('mongo_queries', 1);
-			return $this->i->{$this->table}->remove($cr);
-		}
-		
-		public function asId($id)
-		{
-			return (is_object($id)) ? $id : (new \MongoId($id));
+			return $this->i->{$this->table}->remove($cr)['ok'];
 		}
 		
 	}
